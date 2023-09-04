bd7d24bff8576310ca5519ee16546d58ce2ccf8f
you <sheepboot8718@163.com>
2023-09-04 16:38:53 +0800

    增加premake

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "Astan"
+	architecture "x64" 
+
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Astan"
+	location "Astan"
+	kind "SharedLib"
+	language "C++"
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor\\spdlog\\include"
+	}
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"AS_PLATFORM_WINDOWS",
+			"AS_BUILD_DLL",
+			"_WINDLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Application")
+		}
+
+	filter "configurations:Debug"
+		defines "AS_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "AS_RELEASE"
+		symbols "On"
+
+	filter "configurations:Dist"
+		defines "AS_DIST"
+		symbols "On"
+
+project "Application"
+	location "Application"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Astan/vendor\\spdlog\\include",
+		"Astan/src"
+	}
+
+	links
+	{
+		"Astan"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"AS_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "AS_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "AS_RELEASE"
+		symbols "On"
+
+	filter "configurations:Dist"
+		defines "AS_DIST"
+		symbols "On"
