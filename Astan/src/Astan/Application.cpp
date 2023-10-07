d5fb40ad026e2132f08e505f2d97806f08f1ab84
you <110140947.com>
2023-10-07 22:28:48 +0800

    P31

--- Astan/src/Astan/Application.cpp
+++ Astan/src/Astan/Application.cpp
@@ -10,6 +10,26 @@ namespace Astan {
 
 	Application* Application::s_Instance = nullptr;
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+		case Astan::ShaderDataType::Float:    return GL_FLOAT;
+		case Astan::ShaderDataType::Float2:   return GL_FLOAT;
+		case Astan::ShaderDataType::Float3:   return GL_FLOAT;
+		case Astan::ShaderDataType::Float4:   return GL_FLOAT;
+		case Astan::ShaderDataType::Mat3:     return GL_FLOAT;
+		case Astan::ShaderDataType::Mat4:     return GL_FLOAT;
+		case Astan::ShaderDataType::Int:      return GL_INT;
+		case Astan::ShaderDataType::Int2:	  return GL_INT;
+		case Astan::ShaderDataType::Int3:	  return GL_INT;
+		case Astan::ShaderDataType::Int4:	  return GL_INT;
+		case Astan::ShaderDataType::Bool:	  return GL_BOOL;
+		}
+		AS_CORE_ASSERT(false, "Unknown ShaderDatatype!");
+		return 0;
+	}
+
 	Application::Application()
 	{
 		AS_CORE_ASSERT(!s_Instance, "Application already exists!");
@@ -23,16 +43,36 @@ namespace Astan {
 		glBindVertexArray(m_VertexArray);
 
 
-		float vertices[3 * 3] = {
-			-0.5f,-0.5f,0.0f,
-			0.5f,-0.5f,0.0f,
-			0.0f,0.5f,0.0f
+		float vertices[3 * 7] = {
+			-0.5f,-0.5f,0.0f, 0.8f,0.2f,0.8f,1.0f,
+			 0.5f,-0.5f,0.0f, 0.2f,0.3f,0.8f,1.0f,
+			 0.0f, 0.5f,0.0f, 0.8f,0.8f,0.2f,1.0f,
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
-		
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		{
+			BufferLayout layout =
+			{
+				{ShaderDataType::Float3, "a_Position"},
+				{ShaderDataType::Float4, "a_Color"}
+			};
+
+			m_VertexBuffer->SetLayout(layout);
+		}
+
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index, element.GetComponentCount(),
+				ShaderDataTypeToOpenGLBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE,
+				layout.GetStride(),
+				(const void*)element.Offset);
+			index++;
+		}
+
 
 
 		uint32_t indices[3] = { 0,1,2 };
@@ -41,11 +81,14 @@ namespace Astan {
 		std::string vertexSource = R"(
 			#version 330 core
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
 			
 			out vec3 v_Position;
+			out vec4 v_Color;
 			void main()
 			{
 				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position,1.0);
 			}
 		)";
@@ -53,10 +96,12 @@ namespace Astan {
 			#version 330 core
 			layout(location = 0) out vec4 color;
 			in vec3 v_Position;
+			in vec4 v_Color;
 
 			void main()
 			{
 				color = vec4(v_Position*0.5+0.5, 1.0);
+				color = v_Color;
 			}
 		)";
 		m_Shader.reset(new Shader(vertexSource, fragmentSource));
--- Astan/src/Platform/OpenGL/OpenGLBuffer.h
+++ Astan/src/Platform/OpenGL/OpenGLBuffer.h
@@ -10,10 +10,13 @@ namespace Astan
 		OpenGLVertexBuffer(float* vertices, uint32_t size);
 		virtual ~OpenGLVertexBuffer();
 
-		virtual void Bind() const;
-		virtual void Unbind() const;
+		virtual void Bind() const override;
+		virtual void Unbind() const override;
+		virtual const BufferLayout& GetLayout() const override { return m_Layout; };
+		virtual void SetLayout(const BufferLayout& layout) override { m_Layout = layout; };
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_Layout;
 	};
 
 	class OpenGLIndexBuffer : public IndexBuffer
@@ -21,8 +24,9 @@ namespace Astan
 	public:
 		OpenGLIndexBuffer(uint32_t* indices, uint32_t count);
 		virtual ~OpenGLIndexBuffer();
-		virtual void Bind() const;
-		virtual void Unbind() const;
+		virtual void Bind() const override;
+		virtual void Unbind() const override;
+
 
 		virtual uint32_t GetCount() const { return m_Count; };
 	private:
