cd472d4cda96d64824e79ea2032a61a2353b1f20
you <sheepboot8718@163.com>
2023-10-17 12:13:59 +0800

    P52

--- Application/src/Sandbox2D.cpp
+++ Application/src/Sandbox2D.cpp
@@ -4,7 +4,6 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include "Platform/OpenGL/OpenGLShader.h"
 
 Sandbox2D::Sandbox2D()
 	:Layer("Sandbox2D"), m_CameraController(1280.0f / 720.0f)
@@ -30,11 +29,10 @@ void Sandbox2D::OnUpdate(Astan::Timestep ts)
 	Astan::RenderCommand::Clear();
 
 	Astan::Renderer2D::BeginScene(m_CameraController.GetCamera());
-	Astan::Renderer2D::DrawQuad({ 0.0f,0.0f }, { 1.0f,1.0f }, { 0.8f,0.2f,0.3f,1.0f });
-	Astan::Renderer2D::EndScene();
+	Astan::Renderer2D::DrawQuad({ -1.0f,0.0f }, { 0.8f,0.8f }, { 0.8f,0.2f,0.3f,1.0f });
+	Astan::Renderer2D::DrawQuad({ 0.5f,-0.5f }, { 0.5f,0.5f }, { 0.2f,0.3f,0.8f,1.0f });
 
-	//std::dynamic_pointer_cast<Astan::OpenGLShader>(m_FlatColorShader)->Bind();
-	//std::dynamic_pointer_cast<Astan::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat4("u_Color", m_SquareColor);
+	Astan::Renderer2D::EndScene();
 
 }
 
