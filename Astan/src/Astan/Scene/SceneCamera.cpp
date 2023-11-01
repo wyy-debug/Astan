28eec8aee1eddda16abd894d1056705be5e668c7
you <sheepboot8718@163.com>
2023-11-01 16:09:51 +0800

    P86

--- Astan/src/Astan/Scene/SceneCamera.cpp
+++ Astan/src/Astan/Scene/SceneCamera.cpp
@@ -12,11 +12,22 @@ namespace Astan
 
 	void SceneCamera::SetOrthographic(float size, float nearClip, float farClip)
 	{
+		m_ProjectionType = ProjectionType::Orthographic;
 		m_OrthographicSize = size;
 		m_OrthographicNear = nearClip;
 		m_OrthographicFar = farClip;
 		RecalculateProjection();
 	}
+
+	void SceneCamera::SetPerspective(float verticalFov, float nearClip, float farClip)
+	{
+		m_ProjectionType = ProjectionType::Perspective;
+		m_PerspectiveFOV = verticalFov;
+		m_PerspectiveNear = nearClip;
+		m_PerspectiveFar = farClip;
+		RecalculateProjection();
+	}
+
 
 	void SceneCamera::SetViewportSize(uint32_t width,uint32_t height)
 	{
@@ -26,11 +37,19 @@ namespace Astan
 
 	void SceneCamera::RecalculateProjection()
 	{
-		float orthoLeft = -m_OrthographicSize * m_AspectRatio * 0.5f;
-		float orthoRight = m_OrthographicSize * m_AspectRatio * 0.5f;
-		float orthoBottom = -m_OrthographicSize * 0.5f;
-		float orthoTop = m_OrthographicSize * 0.5f;
+		if (m_ProjectionType == ProjectionType::Perspective)
+		{
+			m_Projection = glm::perspective(m_PerspectiveFOV, m_AspectRatio, m_PerspectiveNear, m_PerspectiveFar);
+		}
+		else
+		{
+			float orthoLeft = -m_OrthographicSize * m_AspectRatio * 0.5f;
+			float orthoRight = m_OrthographicSize * m_AspectRatio * 0.5f;
+			float orthoBottom = -m_OrthographicSize * 0.5f;
+			float orthoTop = m_OrthographicSize * 0.5f;
 
-		m_Projection = glm::ortho(orthoLeft, orthoRight, orthoBottom, orthoTop, m_OrthographicNear, m_OrthographicFar);
+			m_Projection = glm::ortho(orthoLeft, orthoRight, orthoBottom, orthoTop, m_OrthographicNear, m_OrthographicFar);
+		}
+		
 	}
 }
--- Astan/src/Astan/Scene/SceneCamera.h
+++ Astan/src/Astan/Scene/SceneCamera.h
@@ -7,21 +7,47 @@ namespace Astan
 	class SceneCamera : public Camera
 	{
 	public:
+		enum class ProjectionType { Perspective = 0, Orthographic = 1};
+	public:
 		SceneCamera();
 		virtual ~SceneCamera() = default;
 
 		void SetOrthographic(float size, float nearClip, float farClip);
+		void SetPerspective(float verticalFov, float nearClip, float farClip);
 
 		void SetViewportSize(uint32_t width, uint32_t height);
 
+		float GetPerspectiveVerticalFOV() const { return m_PerspectiveFOV; }
+		void SetPerspectiveVerticalFOV(float verticalFov) { m_PerspectiveFOV = verticalFov; RecalculateProjection(); }
+		float GetPerspectiveVerticalNearClip() const { return m_PerspectiveNear; }
+		void SetPerspectiveVerticalNearClip(float nearClip) { m_PerspectiveNear = nearClip; RecalculateProjection(); }
+		float GetPerspectiveVerticalFarClip() const { return m_PerspectiveFar; }
+		void SetPerspectiveVerticalFarClip(float farClip) { m_PerspectiveFar = farClip; RecalculateProjection(); }
+
+
 		float GetOrthographicSzie() const { return m_OrthographicSize; }
 		void SetOrthographicSzie(float size) { m_OrthographicSize = size; RecalculateProjection(); }
+		float GetOrthographicNearClip() const { return m_OrthographicNear; }
+		void SetOrthographicNearClip(float nearClip) { m_OrthographicNear = nearClip; RecalculateProjection(); }
+		float GetOrthographicFarClip() const { return m_OrthographicFar; }
+		void SetOrthographicFarClip(float farClip) { m_OrthographicFar = farClip; RecalculateProjection(); }
+
+
+		ProjectionType GetProjectionType() const { return m_ProjectionType; }
+		void SetProjectionType(ProjectionType type) { m_ProjectionType = type; RecalculateProjection();} 
 	private:
 		void RecalculateProjection();
 	private:
+		ProjectionType m_ProjectionType = ProjectionType::Orthographic;
+
 		float m_OrthographicSize = 10.0f;
 		float m_OrthographicNear = -1.0f;
 		float m_OrthographicFar = 1.0f;
+
+
+		float m_PerspectiveFOV = glm::radians(45.0f);
+		float m_PerspectiveNear = 0.01f;
+		float m_PerspectiveFar = 1000.0f;
 
 		float m_AspectRatio = 0.0f;
 	};
