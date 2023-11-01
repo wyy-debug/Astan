61ca3f4d79098e8cc458d89db6639e1759393fcf
you <sheepboot8718@163.com>
2023-11-01 11:31:03 +0800

    P85

--- Astannut/src/Panels/SceneHierarchyPanel.cpp
+++ Astannut/src/Panels/SceneHierarchyPanel.cpp
@@ -1,5 +1,6 @@
 #include "SceneHierarchyPanel.h"
 #include <imgui/imgui.h>
+#include <glm/gtc/type_ptr.hpp>
 #include "Astan/Scene/Component.h"
 
 namespace Astan
@@ -25,6 +26,18 @@ namespace Astan
 			DrawEntityNode(entity);
 		});
 
+
+		if (ImGui::IsMouseDown(0) && ImGui::IsWindowHovered())
+			m_SelectionContext = {};
+
+		ImGui::End();
+
+
+		ImGui::Begin("Properties");
+		if (m_SelectionContext)
+		{
+			DrawComponents(m_SelectionContext);
+		}
 		ImGui::End();
 	}
 
@@ -48,6 +61,32 @@ namespace Astan
 			if (opened)
 				ImGui::TreePop();
 			ImGui::TreePop();
+		}
+	}
+
+	void SceneHierarchyPanel::DrawComponents(Entity entity)
+	{
+		if (entity.HasComponent<TagComponent>())
+		{
+			auto& tag = entity.GetComponent<TagComponent>().Tag;
+			
+			char buffer[256];
+			memset(buffer, 0, sizeof(buffer));
+			strcpy_s(buffer, 256, tag.c_str());
+			if (ImGui::InputText("Tag", buffer, sizeof(buffer)));
+			{
+				tag = std::string(buffer);
+			}
+		}
+
+		if (entity.HasComponent<TransformComponent>())
+		{
+			if(ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen,"Transform"))
+			{
+				auto& transform = entity.GetComponent<TransformComponent>().Transform;
+				ImGui::DragFloat3("Position", glm::value_ptr(transform[3]), 0.1f);
+				ImGui::TreePop();
+			}
 		}
 	}
 }
