2cbc6791fd26500e9f939fa3d92db2145e994e30
you <sheepboot8718@163.com>
2023-11-07 14:35:11 +0800

    [fix]P93

--- Astan/src/Astan/Math/Math.cpp
+++ Astan/src/Astan/Math/Math.cpp
@@ -40,13 +40,13 @@ namespace Astan::Math
 		scale.y = length(Row[1]);
 		Row[1] = detail::scale(Row[1], static_cast<T>(1));
 		scale.z = length(Row[2]);
-		Row[2] = detail::scale(Row[2], static_cast<T>(2));
+		Row[2] = detail::scale(Row[2], static_cast<T>(1));
 
 		rotation.y = asin(-Row[0][2]);
 		if (cos(rotation.y) != 0)
 		{
 			rotation.x = atan2(Row[1][2], Row[2][2]);
-			rotation.z = atan2(Row[1][2], Row[2][2]);
+			rotation.z = atan2(Row[0][1], Row[0][0]);
 		}
 		else
 		{
