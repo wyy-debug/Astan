e0f74c69b17f6865bb20780ba3983bc862a2dc59
you <sheepboot8718@163.com>
2023-11-13 18:13:41 +0800

    fix

--- premake5.lua
+++ premake5.lua
@@ -1,3 +1,5 @@
+include "./vendor/premake/premake_customization/solution_items.lua"
+include "Dependencies.lua"
 workspace "Astan"
 	architecture "x64" 
 
@@ -10,22 +12,17 @@ workspace "Astan"
 		"Dist"
 	}
 
+	solution_items
+	{
+		".editorconfig"
+	}
+
 	flags
 	{
 		"MultiProcessorCompile"
 	}
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
-
-IncludeDir = {}
-IncludeDir["GLFW"] = "Astan/vendor/GLFW/include"
-IncludeDir["Glad"] = "Astan/vendor/Glad/include"
-IncludeDir["ImGui"] = "Astan/vendor/imgui"
-IncludeDir["glm"] = "Astan/vendor/glm"
-IncludeDir["stb_image"] = "Astan/vendor/stb_image"
-IncludeDir["entt"] = "Astan/vendor/entt/include"
-IncludeDir["yamlcpp"] = "Astan/vendor/yaml-cpp/include"
-IncludeDir["ImGuizmo"] = "Astan/vendor/ImGuizmo"
 
 group "Dependencies"
 	include "Astan/vendor/GLFW"
@@ -34,194 +31,6 @@ group "Dependencies"
 	include "Astan/vendor/yaml-cpp"
 group ""
 
-project "Astan"
-	location "Astan"
-	kind "StaticLib"
-	language "C++"
-	cppdialect "C++17"
-	staticruntime "on"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}") 
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-	pchheader "aspch.h"
-	pchsource "Astan/src/aspch.cpp"
-
-	files
-	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp",
-		"%{prj.name}/vendor/stb_image/**.cpp",
-		"%{prj.name}/vendor/stb_image/**.h",
-
-		"%{prj.name}/vendor/ImGuizmo/ImGuizmo.h",
-		"%{prj.name}/vendor/ImGuizmo/ImGuizmo.cpp"
-	}
-
-	defines
-	{
-		"_CRT_SECURE_NO_WARNINGS"
-	}
-
-	includedirs
-	{
-		"%{prj.name}/vendor\\spdlog\\include",
-		"%{prj.name}/src",
-		"%{IncludeDir.GLFW}",
-		"%{IncludeDir.yamlcpp}",
-		"%{IncludeDir.Glad}",
-		"%{IncludeDir.ImGui}",
-		"%{IncludeDir.glm}",
-		"%{IncludeDir.stb_image}",
-		"%{IncludeDir.entt}",
-		"%{IncludeDir.ImGuizmo}"
-
-	}
-	links
-	{
-		"GLFW",
-		"Glad",
-		"ImGui",
-		"yaml-cpp",
-		"opengl32.lib",
-		"Dwmapi.lib"
-	}
-
-
-	filter "system:windows"
-		systemversion "latest"
-
-		defines
-		{
-			"AS_PLATFORM_WINDOWS",
-			"AS_BUILD_DLL",
-			"_WINDLL",
-			"GLFW_INCLUDE_NONE"
-		}
-
-	filter "configurations:Debug"
-		defines "AS_DEBUG"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines "AS_RELEASE"
-		runtime "Release"
-		symbols "on"
-
-	filter "configurations:Dist"
-		defines "AS_DIST"
-		runtime "Release"
-		symbols "on"
-
-project "Application"
-	location "Application"
-	kind "ConsoleApp"
-	language "C++"
-	cppdialect "C++17"
-	staticruntime "on"
-
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-	files
-	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
-	}
-
-	includedirs
-	{
-		"Astan/vendor\\spdlog\\include",
-		"Astan/src",
-		"Astan/vendor",
-		"%{IncludeDir.glm}"	,
-		"%{IncludeDir.entt}",
-	}
-
-	links
-	{
-		"Astan"
-	}
-
-	filter "system:windows"
-
-		systemversion "latest"
-
-		defines
-		{
-			"AS_PLATFORM_WINDOWS"
-		}
-
-	filter "configurations:Debug"
-		defines "AS_DEBUG"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines "AS_RELEASE"
-		runtime "Release"
-		symbols "on"
-
-	filter "configurations:Dist"
-		defines "AS_DIST"
-		runtime "Release"
-		symbols "on"
-
-project "Astannut"
-	location "Astannut"
-	kind "ConsoleApp"
-	language "C++"
-	cppdialect "C++17"
-	staticruntime "on"
-
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-	files
-	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
-	}
-
-	includedirs
-	{
-		"Astan/vendor\\spdlog\\include",
-		"Astan/src",
-		"Astan/vendor",
-		"%{IncludeDir.glm}",
-		"%{IncludeDir.entt}",
-		"%{IncludeDir.ImGuizmo}"
-
-	}
-
-	links
-	{
-		"Astan"
-	}
-
-	filter "system:windows"
-
-		systemversion "latest"
-
-		defines
-		{
-			"AS_PLATFORM_WINDOWS"
-		}
-
-	filter "configurations:Debug"
-		defines "AS_DEBUG"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines "AS_RELEASE"
-		runtime "Release"
-		symbols "on"
-
-	filter "configurations:Dist"
-		defines "AS_DIST"
-		runtime "Release"
-		symbols "on"
+include "Astan"
+include "Application"
+include "Astannut"
